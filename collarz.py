7b56fcc77e27712f116084b5a4b213c61eb22cf0
obzerving <observing@gmail.com>
2021-07-09 11:42:58 -0400

    Change orientation parameter
    testHt changed to 0.1% of currTabHt

--- collarz.py
+++ collarz.py
@@ -236,7 +236,7 @@ class Collar(inkex.EffectExtension):
         currTabHt = tabht
         currTabAngle = taba
         testAngle = 1.0
-        testHt = currTabHt * 0.01
+        testHt = currTabHt * 0.001
         adjustTab = 0
         tabDone = False
         while not tabDone:
